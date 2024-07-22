cc25f1bfa83950b74cd7971c83a94057110a4453
jhillacre <joel@403forbidden.ca>
2024-07-22 15:50:54 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <img src="./assets/primevue-logo.png" alt="PrimeVue Logo" />
+  <h2>PrimeVue Issue Template</h2>
+  <p>
+    Please create a test case and attach the link of the codesandbox to your
+    github issue report.
+  </p>
+  <Button label="default" v-tooltip="default" />
+  <Button label="left" severity="danger" v-tooltip.left="left" />
+  <Button label="right" severity="warning" v-tooltip.right="right" />
+  <Button label="top" severity="info" v-tooltip.top="top" />
+  <Button label="bottom" severity="success" v-tooltip.bottom="bottom" />
+</template>
